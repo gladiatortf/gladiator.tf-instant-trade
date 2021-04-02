05658b7105a096a2e18cb6ae859bbe17af140374
mninc <manicminer106@gmail.com>
2021-04-02 19:53:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Gladiator.tf Instant Trade
+
+Start a trade with a [Gladiator.tf](https://gladiator.tf) bot in a single click.
+
+## Usage
+A "Gladiator.tf Instant Trade" button will appear on every listing from a Gladiator.tf bot. Click it, and in a few seconds you will be directed to the trade offer.
+![Example](https://cdn.discordapp.com/attachments/445162468541464576/827498192546955334/unknown.png)
+
+
+## Requirements
+There are a few requirements for Gladiator.tf to work.
+
+- You must have Steam running on your machine. This is so the bot can automatically be added as a friend, allowing it to send you a trade offer.
+- You must be signed in to Gladiator.tf. The script will direct you to sign in if you are not.
+
+## Installation
+
+1. Install [Tampermonkey](https://www.tampermonkey.net/).
+2. Open the [script in raw view](https://github.com/mninc/gladiator.tf-instant-trade/raw/master/gladiator-instant-trade.user.js).
+3. Install.
---
+++ gladiator-instant-trade.user.js
@@ -0,0 +1,92 @@
+// ==UserScript==
+// @name         Gladiator.tf Instant Trade
+// @namespace    https://steamcommunity.com/profiles/76561198320810968
+// @version      0.1
+// @author       manic
+// @grant        GM_xmlhttpRequest
+// @connect      gladiator.tf
+// @license      MIT
+
+// @homepageURL     https://github.com/mninc/gladiator.tf-instant-trade
+// @supportURL      https://github.com/mninc/gladiator.tf-instant-trade/issues
+// @downloadURL     https://github.com/mninc/gladiator.tf-instant-trade/raw/master/gladiator-instant-trade.user.js
+
+// @run-at       document-end
+// @include      /^https?:\/\/(.*\.)?backpack\.tf(:\d+)?\//
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    const URL = "https://gladiator.tf";
+    let data = localStorage.getItem("gladiator.tf bots");
+    if (data) {
+        data = JSON.parse(data);
+        if (new Date() - new Date(data.at) < 1000 * 60 * 60 * 24 && data.url === URL) return addLinks(data.bots);
+    }
+    console.log("fetching bots");
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: `${URL}/api/bots`,
+        onload: function (data) {
+            data = JSON.parse(data.responseText);
+            if (!data.success) return alert(data.message);
+
+            let bots = data.bots;
+            addLinks(bots);
+            localStorage.setItem("gladiator.tf bots", JSON.stringify({at: new Date(), bots, url: URL}));
+        }
+    });
+
+    function addLinks(bots) {
+        const spinner = `<i class="fa fa-spin fa-spinner"></i>`;
+
+        let currentlyTrading = false;
+        $('.listing').each(function () {
+            let listing = $(this);
+            let bot = listing.find('.user-link').data("id");
+            if (!bots.includes(bot)) return;
+            let item = listing.find('.listing-item .item');
+            let buttons = listing.find('.listing-buttons');
+            let instantTrade = $(`<a href='steam://friends/add/${bot}' title='Gladiator.tf Instant Trade' class='btn btn-success btn-bottom btn-xs' data-tip=top style=""></a>`);
+            instantTrade.css("height", "22px");
+            instantTrade.css("width", "23px");
+            instantTrade.css("background-image", "url(https://gladiator.tf/img/logo.svg)");
+            instantTrade.css("background-size", "50%");
+            instantTrade.css("background-repeat", "no-repeat");
+            instantTrade.css("background-position", "center");
+            buttons.append(instantTrade);
+            instantTrade.click(() => {
+                if (currentlyTrading) return Modal.render("Error creating trade", "You already have a trade processing! Wait for it to finish before starting another.");
+                console.log("requesting");
+                currentlyTrading = true;
+                let cart = {buy: [], sell: []};
+                if (item.data("listing_intent") === "buy") cart.sell.push(item.attr("title"));
+                else cart.buy.push(item.attr("title"));
+                instantTrade.html(spinner);
+                instantTrade.css("background-image", "none");
+                GM_xmlhttpRequest({
+                    method: "POST",
+                    url: `${URL}/api/start_trade`,
+                    data: JSON.stringify({bot, cart}),
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    onload: function (data) {
+                        currentlyTrading = false;
+                        data = JSON.parse(data.responseText);
+                        console.log(data);
+                        instantTrade.empty();
+                        instantTrade.css("background-image", "url(https://gladiator.tf/img/logo.svg)");
+                        if (data.success) window.open(data.tradeOfferURL);
+                        else {
+                            if (data.error === "Not signed in") window.open(`${URL}/auth/steam`);
+                            else Modal.render("Error creating trade", data.message);
+                        }
+                    }
+                })
+            })
+        })
+    }
+})();
